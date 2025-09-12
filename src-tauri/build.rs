8d583ff206625d33831982d2c8771aa5340d4113
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-09-12 15:12:58 +0900

    chore: defsを結合する

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,27 +1,43 @@
 fn main() -> anyhow::Result<()> {
-    generate_struct(
-        "generated_manifest.rs",
-        "MANIFEST_SCHEMA_PATH",
-        include_str!("../docs/public/schema/Manifest.json"),
-    )?;
-    generate_struct(
-        "generated_registry.rs",
-        "REGISTRY_SCHEMA_PATH",
-        include_str!("../docs/public/schema/Registry.json"),
-    )?;
+    generate_struct()?;
     tauri_build::build();
 
     Ok(())
 }
 
-fn generate_struct(filename: &str, env_var_name: &str, schema: &str) -> anyhow::Result<()> {
+fn generate_struct() -> anyhow::Result<()> {
     // https://github.com/oxidecomputer/typify/issues/889
     let mut settings = typify::TypeSpaceSettings::default();
     settings.with_crate("super", typify::CrateVers::Any, None);
     let mut typespace = typify::TypeSpace::new(&settings);
-    typespace.add_root_schema(serde_json::from_str(schema)?)?;
+
+    let manifest_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
+    let schema_dir = manifest_dir.join("../docs/public/schema");
+
+    let mut defs = std::collections::HashMap::new();
+    for schema in std::fs::read_dir(&schema_dir)? {
+        let schema = schema?;
+        if schema.file_type()?.is_file() {
+            let json = std::fs::read_to_string(schema.path())?;
+            let root: schemars::schema::RootSchema = serde_json::from_str(&json)?;
+            for (k, v) in root.definitions.clone() {
+                defs.insert(k, v);
+            }
+        }
+    }
+    typespace.add_ref_types(defs)?;
+
+    for schema in std::fs::read_dir(&schema_dir)? {
+        let schema = schema?;
+        if schema.file_type()?.is_file() {
+            let json = std::fs::read_to_string(schema.path())?;
+            let root: schemars::schema::Schema = serde_json::from_str(&json)?;
+            typespace.add_type(&root)?;
+        }
+    }
+
     let out_dir = std::env::var("OUT_DIR")?;
-    let out_path = std::path::Path::new(&out_dir).join(filename);
+    let out_path = std::path::Path::new(&out_dir).join("schema.generated.rs");
     std::fs::write(
         &out_path,
         typespace
@@ -29,6 +45,6 @@ fn generate_struct(filename: &str, env_var_name: &str, schema: &str) -> anyhow::
             .to_string()
             .replace(":: super", "super"),
     )?;
-    println!("cargo::rustc-env={}={}", env_var_name, out_path.display());
+    println!("cargo::rustc-env=SCHEMA_RS_PATH={}", out_path.display());
     Ok(())
 }
--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,10 +1,7 @@
-pub mod manifest {
-    include!(env!("MANIFEST_SCHEMA_PATH"));
+mod generated {
+    include!(env!("SCHEMA_RS_PATH"));
 }
-
-pub mod registry {
-    include!(env!("REGISTRY_SCHEMA_PATH"));
-}
+pub use generated::*;
 
 mod num {
     #[allow(non_camel_case_types)]
@@ -55,7 +52,7 @@ mod tests {
 
     #[test]
     fn test_parsing_example_manifest() {
-        insta::assert_debug_snapshot!(serde_yml::from_str::<manifest::Manifest>(include_str!(
+        insta::assert_debug_snapshot!(serde_yml::from_str::<Manifest>(include_str!(
             "../../docs/examples/manifest.yml"
         ))
         .unwrap());
@@ -63,7 +60,7 @@ mod tests {
 
     #[test]
     fn test_parsing_example_registry() {
-        insta::assert_debug_snapshot!(serde_yml::from_str::<registry::Registry>(include_str!(
+        insta::assert_debug_snapshot!(serde_yml::from_str::<Registry>(include_str!(
             "../../docs/examples/registry.yml"
         ))
         .unwrap());
